e747b907f499623d5640e6a5c92ed062cdf1f2c9
bore-prog <philippeaurore@orange.fr>
2024-04-05 16:12:49 +0200

    :bulb: ADD Theme comment
    If we want to update the theme (like colors, sizes and fonts) of the Spectacle slides

--- prez-aurore-github/index.tsx
+++ prez-aurore-github/index.tsx
@@ -3,6 +3,22 @@ import { createRoot } from 'react-dom/client';
 import { Deck, DefaultTemplate, MarkdownSlideSet } from 'spectacle';
 import mdContent from './slides.md';
 
+// const theme = {
+//   colors: {
+//     primary: '#f00',
+//     secondary: '#00f',
+//     ternary: '#0f0',
+//     quaternary: '#000',
+//     quinary: '#fff'
+//   },
+//   fontSize: {
+//     header: '64px',
+//     paragraph: '28px'
+//   }
+// };
+
+// ajouter theme={theme} à la fin de <Deck>
+
 const Presentation = () => (
   <Deck template={() => <DefaultTemplate />}>
     <MarkdownSlideSet>{mdContent}</MarkdownSlideSet>
